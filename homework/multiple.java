3d28133059b509384e650481739e754d100ba8cb
Nwanyanwu <excel@Nwanyanwus-MacBook-Pro.local>
2022-10-04 20:01:06 -0700

    assignment
    multiplication

---
+++ homework/multiple.java
@@ -0,0 +1,98 @@
+package homework;
+
+public class multiple {
+    public static void main(String... args){
+        System.out.println("Multiplication table for 2,3,4,200 and 2,000,000,000");
+        byte small = 2;
+        byte small3 = 3;
+        byte small4 = 4;
+        short nope = 200;
+        int  bigNo = 2_000_000_000;
+
+        System.out.println("Multiples of 2");
+
+        System.out.println();
+
+        System.out.println("2 * 1 = "+ small * 1);
+        System.out.println("2 * 2 = "+ small * 2);
+        System.out.println("2 * 3 = "+ small * 3);
+        System.out.println("2 * 4 = "+ small * 4);
+        System.out.println("2 * 5 = "+ small * 5);
+        System.out.println("2 * 6 = "+ small * 6);
+        System.out.println("2 * 7 = "+ small * 7);
+        System.out.println("2 * 8 = "+ small * 8);
+        System.out.println("2 * 9 = "+ small * 9);
+        System.out.println("2 * 10 = "+ small * 10);
+
+        System.out.println();
+
+        System.out.println("Multiples of 3");
+
+        System.out.println();
+
+        System.out.println("3 * 1 = " + small3 * 1);
+        System.out.println("3 * 2 = " + small3 * 2);
+        System.out.println("3 * 3 = " + small3 * 3);
+        System.out.println("3 * 4 = " + small3 * 4);
+        System.out.println("3 * 5 = " + small3 * 5);
+        System.out.println("3 * 6 = " + small3 * 6);
+        System.out.println("3 * 7 = " + small3 * 7);
+        System.out.println("3 * 8 = " + small3 * 8);
+        System.out.println("3 * 9 = " + small3 * 9);
+        System.out.println("3 * 10 = " + small3 * 10);
+
+        System.out.println();
+
+        System.out.println("Multiples of 4");
+
+        System.out.println();
+
+        System.out.println("4 * 1 = " + small4 * 1);
+        System.out.println("4 * 2 = " + small4 * 2);
+        System.out.println("4 * 3 = " + small4 * 3);
+        System.out.println("4 * 4 = " + small4 * 4);
+        System.out.println("4 * 5 = " + small4 * 5);
+        System.out.println("4 * 6 = " + small4 * 6);
+        System.out.println("4 * 7 = " + small4 * 7);
+        System.out.println("4 * 8 = " + small4 * 8);
+        System.out.println("4 * 9 = " + small4 * 9);
+        System.out.println("4 * 10 = " + small4 * 10);
+
+        System.out.println();
+
+        System.out.println("Multiples of 200");
+
+        System.out.println();
+
+        System.out.println("200 * 1 = " + nope * 1);
+        System.out.println("200 * 2 = " + nope * 2);
+        System.out.println("200 * 3 = " + nope * 3);
+        System.out.println("200 * 4 = " + nope * 4);
+        System.out.println("200 * 5 = " + nope * 5);
+        System.out.println("200 * 6 = " + nope * 6);
+        System.out.println("200 * 7 = " + nope * 7);
+        System.out.println("200 * 8 = " + nope * 8);
+        System.out.println("200 * 9 = " + nope * 9);
+        System.out.println("200 * 10 = " + nope * 10);
+
+        System.out.println();
+
+        System.out.println("Multiples of 2,000,000,000");
+
+        System.out.println();
+
+        System.out.println("2,000,000,000 * 1 = " + bigNo * 1);
+        System.out.println("2,000,000,000 * 2 = " + (long)bigNo * 2);
+        System.out.println("2,000,000,000 * 3 = " + (long)bigNo * 3);
+        System.out.println("2,000,000,000 * 4 = " + (long)bigNo * 4);
+        System.out.println("2,000,000,000 * 5 = " + (long)bigNo * 5);
+        System.out.println("2,000,000,000 * 6 = " + (long)bigNo * 6);
+        System.out.println("2,000,000,000 * 7 = " + (long)bigNo * 7);
+        System.out.println("2,000,000,000 * 8 = " + (long)bigNo * 8);
+        System.out.println("2,000,000,000 * 9 = " + (long)bigNo * 9);
+        System.out.println("2,000,000,000 * 10 = " + (long)bigNo * 10);
+
+
+
+    }
+}
